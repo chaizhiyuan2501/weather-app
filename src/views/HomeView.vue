bf72a9e6e37c813081a78854636f20d515f1c727
Chai Zhi yuan_NotePC <chaizhiyuan2501@gmail.com>
2025-06-20 21:50:11 +0900

    HomeViewにForecastChartを追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,18 +14,32 @@
                 </v-alert>
             </v-col>
         </v-row>
+        <ForecastChart v-if="forecastData" :labels="forecastData.list.map(item => item.date.dt_txt.slice(5, 16))"
+            :temperatures="forecastData.list.map(item => item.main.temp)" />
     </v-container>
 
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useWeatherStore } from '@/store/weather';
 import WeatherCard from '@/components/WeatherCard.vue';
+import { fetchForecast, useWeather } from "@/composables/useWeather";
+import ForecastChart from '@/components/ForecastChart.vue';
 
 const store = useWeatherStore();
 const { city, weather, error } = storeToRefs(store);
-const { fetchWeather } = store;
+const forecastData = ref<any[]>(null);
+
+const fetchWeather = async () => {
+    const result = await useWeather(city.value, "ja");
+    weather.value = result.data;
+    error.value = result.error as Error;
+
+    const forecast = await fetchForecast(city.value, "ja");
+    forecastData.value = forecast.data;
+};
 
 </script>
 <style scoped></style>
